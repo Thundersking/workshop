f6ac0f65a66f15eacb6b9ad6f00a980c5d6550ea
Daniil <d.a.sherkozhukhov1995@gmail.com>
2020-08-06 21:23:39 +0300

    v1.2

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,63 @@
-$(document).ready(function () {
-	$('.slider').slick({
-		dots:true,
-		prevArrow:false,
-		nextArrow:false,
-	});
-});
 
+//slick slider
+$('.carousel-reviews').slick({
+	centerMode: true,
+	centerPadding: '270px',
+	slidesToShow: 1,
+	variableWidth: true,
+	responsive: [
+	  {
+		breakpoint: 768,
+		settings: {
+		  arrows: false,
+		  centerMode: true,
+		  centerPadding: '40px',
+		  slidesToShow: 3
+		}
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  arrows: false,
+		  centerMode: true,
+		  centerPadding: '40px',
+		  slidesToShow: 1
+		}
+	  }
+	]
+  });
 
-function openbox(blog_button){
-	display = document.getElementById('blog-button').style.display;
-	
-	if(display=='none'){
-		document.getElementById('blog-button').style.display='flex';
-	} else{
-		document.getElementById('blog-button').style.display='none';
+//button up
+  var btn = $('#button');
+
+  $(window).scroll(function() {
+	if ($(window).scrollTop() > 300) {
+	  btn.addClass('show');
+	} else {
+	  btn.removeClass('show');
 	}
-}
+  });
+  
+  btn.on('click', function(e) {
+	e.preventDefault();
+	$('html, body').animate({scrollTop:0}, '300');
+  });
+  
+  
+
+//timer
+var countDownDate = new Date('00:00:00').getTime();
+
+var countDownFunction = setInterval(function () {
+
+	var now = new Date().getTime();
+
+	var distance = countDownDate - now;
+
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	document.getElementById('timer').innerHTML = 
+		hours + 'ч ' + minutes + 'м ' + seconds + 'с ';
+}, 1000);
